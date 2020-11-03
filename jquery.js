bf0565dc5553defc33a9e5ed7045befe1ccfda96
Shahzod <shahzodtoatov@gmail.com>
2020-11-03 20:53:40 +0500

    1

---
+++ jquery.js
@@ -0,0 +1,65 @@
+$(function () {
+    $('.slider').slick({
+        infinite: true,  //Слайдер будет бесконечным
+        slidesToShow: 6, //Сколько слайдов будет показанно на экране
+        slidesToScroll: 1,  //Сколько слайдов переходить        
+        autoplay: true,
+        autoplaySpeed: 3000,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 576,  //small
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                // dots: true
+              }
+            },
+            {
+                breakpoint: 500,  //small
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                // dots: true
+              }
+            },
+            {
+                  breakpoint: 768,  //medium
+                    settings: {
+                      slidesToShow: 3,
+                      slidesToScroll: 1,
+                      infinite: true,
+                    }
+                  },
+                  {
+                  breakpoint: 992,  //large
+                    settings: {
+                      slidesToShow: 3,
+                      slidesToScroll: 1,
+                      infinite: true,
+                    }
+          },
+          {
+            breakpoint: 1200,  //extra large
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+              }
+            },
+        ]
+        
+    })
+})
+
+$(function () {
+    $('.card-img').slick({
+        infinite: true,  //Слайдер будет бесконечным
+        slidesToShow: 1, //Сколько слайдов будет показанно на экране
+        slidesToScroll: 1,  //Сколько слайдов переходить        
+        autoplay: true,
+        autoplaySpeed: 3000,
+    })
+})
